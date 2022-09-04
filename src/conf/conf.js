52b0a8eeba4f67cd901ad3cdd89bbb773b0d0c25
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-09-04 19:15:27 +0800

    Links

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,12 +1,14 @@
 import youtube from '../assets/youtube-brands.svg';
-import tiktok from '../assets/tiktok-brands.svg';
+import twitter from '../assets/twitter.svg';
+import discord from '../assets/discord.svg';
 import _abi_raceContract from './abi_raceContract.json';
 
 export const
 	twitchURL                 = 'https://player.twitch.tv/?channel=redlinedao&parent=' + window.location.host.split( ':' )[0],
 	rewardsLinks              = [
-		[<img height='25' alt='Youtube' src={youtube}/>, '#'],
-		[<img height='25' alt='Tiktok' src={tiktok}/>, '#'],
+		[<img height='25' alt='Twitter' src={twitter}/>, 'https://twitter.com/PlayRedline'],
+		[<img height='25' alt='Discord channel' src={discord}/>, 'https://discord.gg/UKmSwPrKjw'],
+		[<img height='25' alt='Youtube' src='https://crew3.xyz/static/media/Crew3_150x47.5.40902c36d4459e858b9b31d1deee26e0.svg'/>, 'https://redline.crew3.xyz/'],
 	],
 	sponsorLinks              = [
 		['isotropic', '#'],
