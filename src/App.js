b20d8e98a19d66c8d5f173a734696d63f09dcabf
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-10-18 04:53:04 +0800

    NFT fetch and refactor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from './layout/Header'
 import Home from './pages/Home'
+import Workshop from './pages/Workshop'
 import AppContext from './app/AppContext';
 import {useEffect, useState} from "react";
+import {fetchNFTsFromAspect} from "./services/data";
 
 function App() {
-	const [ wallet, setWallet ] = useState();
-	useEffect( () => {}, [] );
+	const [context, _setContext] = useState( {
+		wallet: {},
+		nfts  : null,
+	} );
+	const fetchNFTs = () => {
+		const {selectedAddress} = context.wallet;
+		fetchNFTsFromAspect( selectedAddress ).then( nfts => {
+			const partNFTs = [], skinNFTs = [], robotNFTs = [], allNFTs = [];
+			nfts.forEach( ( {token_id, name, description, attributes, image_uri} ) => {
+				const nftData = {
+					token_id, description, image_uri,
+					name      : name.replace( 'Redline Alpha: ', '' ),
+					attributes: {}
+				};
+				attributes.forEach( ( {trait_type, value} ) => {
+					if ( value && value !== '0' ) {
+						nftData.attributes[trait_type.replace( ' ', '' )] = value
+					}
+				} );
+				allNFTs.push( nftData );
+				if ( nftData.attributes['PartType'] ) {
+					partNFTs.push( nftData );
+				} else if ( nftData.attributes['SkinType'] ) {
+					skinNFTs.push( nftData );
+				} else {
+					robotNFTs.push( nftData );
+				}
+			} );
+			setContext( {allNFTs, partNFTs, skinNFTs, robotNFTs} )
+		} );
+	}
+
+	function setContext( newContext ) {
+		_setContext( {...context, ...newContext} )
+	}
+
 	return (
 		<BrowserRouter>
-			<AppContext.Provider value={{
-					wallet,
-					isConnected: !!wallet?.isConnected,
-					setWallet
-				}}>
+			<AppContext.Provider value={{...context, setContext, fetchNFTs}}>
 				<div className="App min-vh-100 flex flex-column w-100 bg-near-black near-white">
 					<header className="dt w-100 border-box pa3 ph4-ns bb b--white-10">
 						<Header/>
@@ -23,14 +55,15 @@ function App() {
 						<Routes>
 							<Route path="/">
 								<Route index element={<Home/>}/>
-								<Route path="workshop" element={<Workshop />}/>
+								<Route path="workshop" element={<Workshop/>}/>
 								{/*<Route path={'*'} element={<Home/>}/>*/}
 								{/*<Route path="*" element={<NoPage />} />*/}
 							</Route>
-
+						</Routes>
 					</section>
 				</div>
 			</AppContext.Provider>
+		</BrowserRouter>
 	);
 }
 
