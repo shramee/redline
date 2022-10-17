45f964ee5ce8213f505559fdfa700e3dd4f8036c
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-10-18 00:52:02 +0800

    Routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from './layout/Header'
 import Home from './pages/Home'
 import AppContext from './app/AppContext';
@@ -7,21 +8,29 @@ function App() {
 	const [ wallet, setWallet ] = useState();
 	useEffect( () => {}, [] );
 	return (
-		<AppContext.Provider value={{
-			wallet,
-			isConnected: !!wallet?.isConnected,
-			setWallet
-		}}>
-			<div className="App min-vh-100 flex flex-column w-100 bg-near-black near-white">
-				<header className="dt w-100 border-box pa3 ph4-ns bb b--white-10">
-					<Header/>
-				</header>
+		<BrowserRouter>
+			<AppContext.Provider value={{
+					wallet,
+					isConnected: !!wallet?.isConnected,
+					setWallet
+				}}>
+				<div className="App min-vh-100 flex flex-column w-100 bg-near-black near-white">
+					<header className="dt w-100 border-box pa3 ph4-ns bb b--white-10">
+						<Header/>
+					</header>
 
-				<section className='w-100 pa3 ph4-ns'>
-					<Home/>
-				</section>
-			</div>
-		</AppContext.Provider>
+					<section className='w-100 pa3 ph4-ns'>
+						<Routes>
+							<Route path="/">
+								<Route index element={<Home/>}/>
+								<Route path="workshop" element={<Workshop />}/>
+								{/*<Route path={'*'} element={<Home/>}/>*/}
+								{/*<Route path="*" element={<NoPage />} />*/}
+							</Route>
+
+					</section>
+				</div>
+			</AppContext.Provider>
 	);
 }
 
