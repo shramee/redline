d61f9f0994138c7c684657960493016de23ee634
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-10-24 00:18:31 +0800

    appStateHOC fetch NFTs

--- src/components/appStateHOC.js
+++ src/components/appStateHOC.js
@@ -1,10 +1,18 @@
 import AppContext from "../app/AppContext";
+import {useEffect} from "react";
 
-export function appStateHOC( WrappedComponent ) {
-	return ( props ) =>
-		<AppContext.Consumer>
-			{( state ) => {
-				return <WrappedComponent {...props} {...state} />
-			}}
-		</AppContext.Consumer>
+export function appStateHOC( WrappedComponent, prefs = {} ) {
+	function InnerWrapper( props ) {
+		const {fetchNFTs, allNFTs, wallet} = props;
+
+		// Fetch NFTs if needed
+		useEffect( () => {
+			prefs.fetchNFTs && !allNFTs?.length && fetchNFTs()
+		}, [wallet?.selectedAddress] );
+		return <WrappedComponent {...props} />
+	}
+
+	return ( props ) => {
+		return <AppContext.Consumer>{state => <InnerWrapper {...state} {...props} />}</AppContext.Consumer>
+	}
 }
