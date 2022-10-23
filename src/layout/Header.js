7ccf27971e334267443d83f7b54d079d73f1dd4b
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-10-24 00:20:14 +0800

    Header with routing

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,22 +1,40 @@
 import WalletConnect from '../components/WalletConnect';
 import {Consumer} from "../app/AppContext";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
+import {appStateHOC} from "../components/appStateHOC";
 
-const Header = () => {
-	return <Consumer>
-		{( {setContext}) => {
-			return <>
-				<Link className="dtc v-mid mid-gray link dim w-25" href="/" title="Home">
-					<h1 className="montserrat900 white f3 ma0 v-mid ttu">
-						<img src="https://redline.game/media/logo.png" alt="Logo" style={{height:28, width: 'auto'}}/>
-					</h1>
-				</Link>
-				<nav className="dtc v-mid w-75 tr">
-					<WalletConnect autoConnect={true} onConnectedStatusChange={( isConnected, wallet ) => setContext( {wallet} )}/>
-				</nav>
-			</>;
-		}}
-	</Consumer>;
+const navBarItems = [
+	["/race", "Races"],
+	["/inventory", "Inventory"],
+	["/workshop", "Workshop"],
+];
+
+function pathNameActive( path, activeReturn = true, inactiveReturn = null ) {
+	if ( window.location.pathname === path ) {
+		return activeReturn;
+	}
+	return inactiveReturn;
 }
 
-export default Header;
+const Header = ( {setContext} ) => {
+	const navBtn = 'white link w4 ba pa2 mh2';
+	return <>
+		<Link className="link w-25" to="/" title="Home">
+			<h1 className="montserrat900 white f3 ma0 v-mid ttu">
+				<img className='db' src="https://redline.game/media/logo.png" alt="Logo" style={{height: 28, width: 'auto'}}/>
+			</h1>
+		</Link>
+		<nav className="flex items-center justify-center w-75 ml-auto tc pl4">
+			{navBarItems.map(
+				( [path, label], i ) =>
+					<NavLink key={i} className={`${navBtn}`} to={path}>
+						{label}</NavLink>
+			)}
+			<div className="ml-auto">
+				<WalletConnect autoConnect={true} onConnectedStatusChange={( isConnected, wallet ) => setContext( {wallet} )}/>
+			</div>
+		</nav>
+	</>;
+}
+
+export default appStateHOC( Header );
